5992b8842143f7de668a354769e4100109cfec3a
Z1F <F953364@163.com>
2024-02-21 21:24:57 +0800

    下半部工作队列按键中断驱动

---
+++ linux_Drivers/14_imx6uirq/.work.ko.cmd
@@ -0,0 +1,1 @@
+cmd_/home/zjf/linux/linux_Drivers/14_imx6uirq/work.ko := arm-linux-gnueabihf-ld -EL -r  -T ./scripts/module-common.lds --build-id  -o /home/zjf/linux/linux_Drivers/14_imx6uirq/work.ko /home/zjf/linux/linux_Drivers/14_imx6uirq/work.o /home/zjf/linux/linux_Drivers/14_imx6uirq/work.mod.o
