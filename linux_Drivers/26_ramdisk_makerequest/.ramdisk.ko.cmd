1112aecef48723ed48a330ab0600d62fa71e3b5b
Z1F <F953364@163.com>
2024-03-19 00:18:11 +0800

    块设备创造请求驱动

---
+++ linux_Drivers/26_ramdisk_makerequest/.ramdisk.ko.cmd
@@ -0,0 +1,1 @@
+cmd_/home/zjf/linux/linux_Drivers/26_ramdisk_makerequest/ramdisk.ko := arm-linux-gnueabihf-ld -EL -r  -T ./scripts/module-common.lds --build-id  -o /home/zjf/linux/linux_Drivers/26_ramdisk_makerequest/ramdisk.ko /home/zjf/linux/linux_Drivers/26_ramdisk_makerequest/ramdisk.o /home/zjf/linux/linux_Drivers/26_ramdisk_makerequest/ramdisk.mod.o
---
+++ linux_Drivers/26_ramdisk_makerequest/.tmp_versions/ramdisk.mod
@@ -0,0 +1,2 @@
+/home/zjf/linux/linux_Drivers/26_ramdisk_makerequest/ramdisk.ko
+/home/zjf/linux/linux_Drivers/26_ramdisk_makerequest/ramdisk.o
