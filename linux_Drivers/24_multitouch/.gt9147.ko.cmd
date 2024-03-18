be8af869fc14eb170bc570096cb2a350684e1db9
Z1F <F953364@163.com>
2024-03-19 00:15:38 +0800

    触摸屏驱动

---
+++ linux_Drivers/24_multitouch/.gt9147.ko.cmd
@@ -0,0 +1,1 @@
+cmd_/home/zjf/linux/linux_Drivers/24_multitouch/gt9147.ko := arm-linux-gnueabihf-ld -EL -r  -T ./scripts/module-common.lds --build-id  -o /home/zjf/linux/linux_Drivers/24_multitouch/gt9147.ko /home/zjf/linux/linux_Drivers/24_multitouch/gt9147.o /home/zjf/linux/linux_Drivers/24_multitouch/gt9147.mod.o
---
+++ linux_Drivers/24_multitouch/.tmp_versions/gt9147.mod
@@ -0,0 +1,2 @@
+/home/zjf/linux/linux_Drivers/24_multitouch/gt9147.ko
+/home/zjf/linux/linux_Drivers/24_multitouch/gt9147.o
