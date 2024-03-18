94f908990041ffb1c4f9f115d8e280c3bb21166e
Z1F <F953364@163.com>
2024-03-19 00:17:16 +0800

    驱动块请求队列驱动

---
+++ linux_Drivers/25_randisk_withrequest/.ramdisk.ko.cmd
@@ -0,0 +1,1 @@
+cmd_/home/zjf/linux/linux_Drivers/25_randisk_withrequest/ramdisk.ko := arm-linux-gnueabihf-ld -EL -r  -T ./scripts/module-common.lds --build-id  -o /home/zjf/linux/linux_Drivers/25_randisk_withrequest/ramdisk.ko /home/zjf/linux/linux_Drivers/25_randisk_withrequest/ramdisk.o /home/zjf/linux/linux_Drivers/25_randisk_withrequest/ramdisk.mod.o
---
+++ linux_Drivers/25_randisk_withrequest/.tmp_versions/ramdisk.mod
@@ -0,0 +1,2 @@
+/home/zjf/linux/linux_Drivers/25_randisk_withrequest/ramdisk.ko
+/home/zjf/linux/linux_Drivers/25_randisk_withrequest/ramdisk.o
